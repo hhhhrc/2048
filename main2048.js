3bcbd6d714ffe4df00af469aaa942b99054362b2
胡日查 <1270660449@qq.com>
2018-03-08 18:42:35 +0800

    end1

--- main2048.js
+++ main2048.js
@@ -1,21 +1,19 @@
-/**
- * Created by liuyubobobo on 14-4-11.
- * my site: http://www.liuyubobobo.com
- */
-
-var board = new Array();
-var score = 0;
+//游戏主逻辑
+var board=new Array();
+var score=0;
 var hasConflicted = new Array();
-
 var startx = 0;
 var starty = 0;
 var endx = 0;
 var endy = 0;
 
-$(document).ready(function(){
-    prepareForMobile();
-    newgame();
-});
+$(document).ready(
+	function()
+	{
+	prepareForMobile();
+	newgame();
+	}
+);
 
 function prepareForMobile(){
 
@@ -35,146 +33,140 @@ function prepareForMobile(){
     $('.grid-cell').css('border-radius',0.02*cellSideLength);
 }
 
-function newgame(){
-    //初始化棋盘格
-    init();
-    //在随机两个格子生成数字
-    generateOneNumber();
-    generateOneNumber();
+function newgame()
+{
+	init();//初始化棋盘格
+	generateOneNumber();
+	generateOneNumber();//随机两个格子生成数字
 }
 
 function init(){
-    for( var i = 0 ; i < 4 ; i ++ )
-        for( var j = 0 ; j < 4 ; j ++ ){
+	for(var i=0;i<4;i++)
+		for(var j=0;j<4;j++){
 
-            var gridCell = $('#grid-cell-'+i+"-"+j);
-            gridCell.css('top', getPosTop( i , j ) );
-            gridCell.css('left', getPosLeft( i , j ) );
-        }
+			var gridCell=$('#grid-cell-'+i+"-"+j);
+			gridCell.css('top',getPosTop(i,j));
+			gridCell.css('left',getPosLeft(i,j));
+		}//动态设置格子的top和left属性
 
-    for( var i = 0 ; i < 4 ; i ++ ){
-        board[i] = new Array();
-        hasConflicted[i] = new Array();
-        for( var j = 0 ; j < 4 ; j ++ ){
-            board[i][j] = 0;
-            hasConflicted[i][j] = false;
-        }
-    }
+	for(var i=0;i<4;i++){
+	board[i]=new Array();
+	hasConflicted[i]=new Array();
+	for(var j=0;j<4;j++){
+		board[i][j]=0; 
+		hasConflicted[i][j]=false;
+	}
+}
 
-    updateBoardView();
-
-    score = 0;
+updateBoardView();
+	score=0;
 }
 
 function updateBoardView(){
+	$(".number-cell").remove();
+	for(var i=0;i<4;i++)
+		for(var j=0;j<4;j++){
+			$("#grid-container").append( '<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>');
+			var theNumberCell=$('#number-cell-'+i+'-'+j);
 
-    $(".number-cell").remove();
-    for( var i = 0 ; i < 4 ; i ++ )
-        for( var j = 0 ; j < 4 ; j ++ ){
-            $("#grid-container").append( '<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>' );
-            var theNumberCell = $('#number-cell-'+i+'-'+j);
+			if (board[i][j]==0){
+				theNumberCell.css('width','0px');
+				theNumberCell.css('height','0px');
+				theNumberCell.css('top',getPosTop(i, j)+cellSideLength/2);
+				theNumberCell.css('left',getPosLeft(i, j)+cellSideLength/2);
+			}
+			else{
+				theNumberCell.css('width',cellSideLength);
+				theNumberCell.css('height',cellSideLength);
+				theNumberCell.css('top',getPosTop(i, j));
+				theNumberCell.css('left',getPosLeft(i, j));
+				theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j] ) );
+				theNumberCell.css('color',getNumberColor(board[i][j] ) );
+				theNumberCell.text(board[i][j]);
+			}
+			hasConflicted[i][j]=false;
 
-            if( board[i][j] == 0 ){
-                theNumberCell.css('width','0px');
-                theNumberCell.css('height','0px');
-                theNumberCell.css('top',getPosTop(i,j) + cellSideLength/2 );
-                theNumberCell.css('left',getPosLeft(i,j) + cellSideLength/2 );
-            }
-            else{
-                theNumberCell.css('width',cellSideLength);
-                theNumberCell.css('height',cellSideLength);
-                theNumberCell.css('top',getPosTop(i,j));
-                theNumberCell.css('left',getPosLeft(i,j));
-                theNumberCell.css('background-color',getNumberBackgroundColor( board[i][j] ) );
-                theNumberCell.css('color',getNumberColor( board[i][j] ) );
-                theNumberCell.text( board[i][j] );
-            }
-
-            hasConflicted[i][j] = false;
-        }
+		}
 
     $('.number-cell').css('line-height',cellSideLength+'px');
     $('.number-cell').css('font-size',0.6*cellSideLength+'px');
+
 }
 
 function generateOneNumber(){
+	if (nospace(board) )
+		return false;
 
-    if( nospace( board ) )
-        return false;
+	//随机一个位置
+	var randx=parseInt(Math.floor(Math.random()*4 ) );
+	var randy=parseInt(Math.floor(Math.random()*4 ) );
+	while(true){
+		if(board[randx][randy]==0){
+			break;
+		}
+		randx=parseInt(Math.floor(Math.random()*4 ) );
+		randy=parseInt(Math.floor(Math.random()*4 ) );
+		}
 
-    //随机一个位置
-    var randx = parseInt( Math.floor( Math.random()  * 4 ) );
-    var randy = parseInt( Math.floor( Math.random()  * 4 ) );
+	//随机一个数字
+	var randNumber=Math.random()<0.5?2:4;
 
-    var times = 0;
-    while( times < 50 ){
-        if( board[randx][randy] == 0 )
-            break;
+	//显示出来
+	board[randx][randy]=randNumber;//刚生成的随机数附给相应坐标
+	showNumberWithAnimation(randx,randy,randNumber);
 
-        randx = parseInt( Math.floor( Math.random()  * 4 ) );
-        randy = parseInt( Math.floor( Math.random()  * 4 ) );
-
-        times ++;
-    }
-    if( times == 50 ){
-        for( var i = 0 ; i < 4 ; i ++ )
-            for( var j = 0 ; j < 4 ; j ++ ){
-                if( board[i][j] == 0 ){
-                    randx = i;
-                    randy = j;
-                }
-            }
-    }
-
-    //随机一个数字
-    var randNumber = Math.random() < 0.5 ? 2 : 4;
-
-    //在随机位置显示随机数字
-    board[randx][randy] = randNumber;
-    showNumberWithAnimation( randx , randy , randNumber );
-
-    return true;
+	return true;
 }
 
-$(document).keydown( function( event ){
-
-    switch( event.keyCode ){
-        case 37: //left
-            event.preventDefault();
-            if( moveLeft() ){
-                setTimeout("generateOneNumber()",210);
-                setTimeout("isgameover()",300);
-            }
-            break;
-        case 38: //up
-            event.preventDefault();
-            if( moveUp() ){
-                setTimeout("generateOneNumber()",210);
-                setTimeout("isgameover()",300);
-            }
-            break;
-        case 39: //right
-            event.preventDefault();
-            if( moveRight() ){
-                setTimeout("generateOneNumber()",210);
-                setTimeout("isgameover()",300);
-            }
-            break;
-        case 40: //down
-            event.preventDefault();
-            if( moveDown() ){
-                setTimeout("generateOneNumber()",210);
-                setTimeout("isgameover()",300);
-            }
-            break;
-        default: //default
-            break;
-    }
-});
+$(document).keydown(function(event){
+    event.preventDefault();
+	switch (event.keyCode) {
+			case 37:
+			// left
+			event.preventDefault();
+			if(moveLeft() ){
+				setTimeout("generateOneNumber();",210);
+				setTimeout("isgameover()",300);
+			}
+			break;
+			case 38:
+			// up
+			event.preventDefault();
+			if(moveUp() ){
+				setTimeout("generateOneNumber();",210);
+				setTimeout("isgameover()",300);
+			}
+			break;
+			case 39:
+			// Right
+			event.preventDefault();
+			if(moveRight() ){
+				setTimeout("generateOneNumber();",210);
+				setTimeout("isgameover()",300);
+			}
+			break;
+			case 40:
+			// down
+			event.preventDefault();
+			if(moveDown() ){
+				setTimeout("generateOneNumber();",210);
+				setTimeout("isgameover()",300);
+			}
+			break;
+			//default
+			default:
+			break;
+	}
+}
+);
 
 document.addEventListener('touchstart',function(event){
     startx = event.touches[0].pageX;
     starty = event.touches[0].pageY;
+});
+
+document.addEventListener('touchmove',function(event){
+	event.preventDefault();
 });
 
 document.addEventListener('touchend',function(event){
@@ -184,7 +176,7 @@ document.addEventListener('touchend',function(event){
     var deltax = endx - startx;
     var deltay = endy - starty;
 
-    if( Math.abs( deltax ) < 0.03*documentWidth && Math.abs( deltay ) < 0.03*documentWidth )
+    if( Math.abs( deltax ) < 0.05*documentWidth && Math.abs( deltay ) < 0.05*documentWidth )
         return;
 
     if( Math.abs( deltax ) >= Math.abs( deltay ) ){
@@ -223,52 +215,50 @@ document.addEventListener('touchend',function(event){
 });
 
 function isgameover(){
-    if( nospace( board ) && nomove( board ) ){
-        gameover();
-    }
+	if( nospace(board) && nomove(board) ) {
+	gameover();
+	}
 }
 
 function gameover(){
-    alert('gameover!');
+	alert("Game Over!")
 }
 
 function moveLeft(){
 
-    if( !canMoveLeft( board ) )
-        return false;
+	if(!canMoveLeft(board))
+		return false;
 
-    //moveLeft
-    for( var i = 0 ; i < 4 ; i ++ )
-        for( var j = 1 ; j < 4 ; j ++ ){
+	//moveLeft
+ 	for( var i = 0 ; i < 4 ; i ++ )
+        	for( var j = 1 ; j < 4 ; j ++ ){
             if( board[i][j] != 0 ){
+            	for(var k=0;k<j;k++){
+            		if(board[i][k]==0&&noBlockHorizontal(i,k,j,board)){
+            			//move
+            			showMoveAnimation(i,j,i,k);
+            			board[i][k]=board[i][j];
+            			board[i][j]=0;
+            			continue;
+            		}
+            		else if (board[i][k]==board[i][j]&&noBlockHorizontal(i,k,j,board)&&!hasConflicted[i][k]) {
+            			//move
+            			showMoveAnimation(i,j,i,k);
+            			//add
+            			board[i][k]+=board[i][j];
+            			board[i][j]=0;
+            			//add score
+            			score+=board[i][k];
+            			updateScore(score);
 
-                for( var k = 0 ; k < j ; k ++ ){
-                    if( board[i][k] == 0 && noBlockHorizontal( i , k , j , board ) ){
-                        //move
-                        showMoveAnimation( i , j , i , k );
-                        board[i][k] = board[i][j];
-                        board[i][j] = 0;
-                        continue;
-                    }
-                    else if( board[i][k] == board[i][j] && noBlockHorizontal( i , k , j , board ) && !hasConflicted[i][k] ){
-                        //move
-                        showMoveAnimation( i , j , i , k );
-                        //add
-                        board[i][k] += board[i][j];
-                        board[i][j] = 0;
-                        //add score
-                        score += board[i][k];
-                        updateScore( score );
-
-                        hasConflicted[i][k] = true;
-                        continue;
-                    }
-                }
+            			hasConflicted[i][k]=true;
+            			continue;
+            		}
+            	}
             }
         }
-
     setTimeout("updateBoardView()",200);
-    return true;
+	return true;
 }
 
 function moveRight(){
@@ -282,23 +272,18 @@ function moveRight(){
                 for( var k = 3 ; k > j ; k -- ){
 
                     if( board[i][k] == 0 && noBlockHorizontal( i , j , k , board ) ){
-                        //move
                         showMoveAnimation( i , j , i , k );
                         board[i][k] = board[i][j];
                         board[i][j] = 0;
                         continue;
                     }
-                    else if( board[i][k] == board[i][j] && noBlockHorizontal( i , j , k , board ) && !hasConflicted[i][k] ){
-                        //move
+                    else if( board[i][k] == board[i][j] && noBlockHorizontal( i , j , k , board ) &&!hasConflicted[i][k]){
                         showMoveAnimation( i , j , i , k);
-                        //add
                         board[i][k] += board[i][j];
                         board[i][j] = 0;
-                        //add score
                         score += board[i][k];
                         updateScore( score );
-
-                        hasConflicted[i][k] = true;
+                        hasConflicted[i][k]=true;
                         continue;
                     }
                 }
@@ -321,23 +306,18 @@ function moveUp(){
                 for( var k = 0 ; k < i ; k ++ ){
 
                     if( board[k][j] == 0 && noBlockVertical( j , k , i , board ) ){
-                        //move
                         showMoveAnimation( i , j , k , j );
                         board[k][j] = board[i][j];
                         board[i][j] = 0;
                         continue;
                     }
-                    else if( board[k][j] == board[i][j] && noBlockVertical( j , k , i , board ) && !hasConflicted[k][j] ){
-                        //move
+                    else if( board[k][j] == board[i][j] && noBlockVertical( j , k , i , board ) &&!hasConflicted[k][j]){
                         showMoveAnimation( i , j , k , j );
-                        //add
                         board[k][j] += board[i][j];
                         board[i][j] = 0;
-                        //add score
                         score += board[k][j];
                         updateScore( score );
-
-                        hasConflicted[k][j] = true;
+                        hasConflicted[k][j]=true;
                         continue;
                     }
                 }
@@ -359,23 +339,18 @@ function moveDown(){
                 for( var k = 3 ; k > i ; k -- ){
 
                     if( board[k][j] == 0 && noBlockVertical( j , i , k , board ) ){
-                        //move
                         showMoveAnimation( i , j , k , j );
                         board[k][j] = board[i][j];
                         board[i][j] = 0;
                         continue;
                     }
-                    else if( board[k][j] == board[i][j] && noBlockVertical( j , i , k , board ) && !hasConflicted[k][j] ){
-                        //move
+                    else if( board[k][j] == board[i][j] && noBlockVertical( j , i , k , board ) &&!hasConflicted[k][j]){
                         showMoveAnimation( i , j , k , j );
-                        //add
                         board[k][j] += board[i][j];
                         board[i][j] = 0;
-                        //add score
                         score += board[k][j];
                         updateScore( score );
-
-                        hasConflicted[k][j] = true;
+                        hasConflicted[k][j]=true;
                         continue;
                     }
                 }
